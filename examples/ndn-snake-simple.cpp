570848590138e29750b6a803af6424d388791757
qijianpeng <l_lzp@sina.com>
2020-10-06 16:04:02 +0800

    Add Computation module.

--- examples/ndn-snake-simple.cpp
+++ examples/ndn-snake-simple.cpp
@@ -41,7 +41,7 @@
 #include "ns3/network-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/ndnSIM-module.h"
-
+#include "ns3/computation-module.h"
 namespace ns3 {
 
 /**
@@ -75,21 +75,22 @@ main(int argc, char* argv[])
   // Read optional command-line parameters (e.g., enable visualizer with ./waf --run=<> --visualize
   CommandLine cmd;
   cmd.Parse(argc, argv);
-
+  
   // Creating nodes
   NodeContainer nodes;
   nodes.Create(3);
-
+  ComputationHelper computation;
+  computation.Install(nodes);
   // Connecting nodes using two links
   PointToPointHelper p2p;
   p2p.Install(nodes.Get(0), nodes.Get(1));
   p2p.Install(nodes.Get(1), nodes.Get(2));
-
   // Install NDN stack on all nodes
   ndn::StackHelper ndnHelper;
   ndnHelper.SetDefaultRoutes(true);
   ndnHelper.InstallAll();
-
+  //TODO(qjp) Set configurations to different nodes.
+  //Config::Set("/NodeList/0/$ns3::ndn::ContentStore/MaxSize", UintegerValue (1));
   // Choosing forwarding strategy
   ndn::StrategyChoiceHelper::InstallAll("/", "/localhost/nfd/strategy/multicast");
 
@@ -104,7 +105,7 @@ main(int argc, char* argv[])
   consumerHelper.SetAttribute("Frequency", StringValue("10")); // 10 interests a second
   auto apps = consumerHelper.Install(nodes.Get(0));                        // first node
   apps.Stop(Seconds(10.0)); // stop the consumer app at 10 seconds mark
-  
+
   // Producer
   ndn::AppHelper producerHelper("ns3::ndn::Producer");
   // Producer will reply to all requests starting with /prefix
